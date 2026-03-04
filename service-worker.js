9a4f668181fd0f8f3f03220d85dbedad6322b6e8
arenadointer-afk <arenadointer@gmail.com>
2026-03-03 22:04:12 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,4 @@
-/* === MOTOR DE NOTIFICAÇÕES (FICA ACORDADO EM SEGUNDO PLANO) === */
-importScripts('https://www.gstatic.com/firebasejs/10.10.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.10.0/firebase-messaging-compat.js');
-
-firebase.initializeApp({
-    apiKey: "AIzaSyDDguzJOP5GKqlqf8GW-xdsTCxh1Ha7C7k",
-    authDomain: "sutello-financeiro.firebaseapp.com",
-    projectId: "sutello-financeiro",
-    storageBucket: "sutello-financeiro.firebasestorage.app",
-    messagingSenderId: "460447549653",
-    appId: "1:460447549653:web:a36b0c7d2c2919ff633a5c"
-});
-
-const messaging = firebase.messaging();
-messaging.onBackgroundMessage((payload) => {
-    const titulo = payload.notification.title;
-    const opcoes = {
-        body: payload.notification.body,
-        icon: '/App/icon-192.png',
-        badge: '/App/icon-192.png'
-    };
-    self.registration.showNotification(titulo, opcoes);
-});
-
-/* === SISTEMA DE CACHE E OFFLINE PWA === */
-const CACHE_NAME = "Sutello-Financeiro-V4";
+const CACHE_NAME = "Sutello-Financeiro-V3";
 
 const FILES_TO_CACHE = [
   "/App/",
@@ -59,9 +34,7 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-        // Retorna o arquivo salvo na memória, ou busca na internet se não tiver
-        return response || fetch(event.request);
+      return response || fetch(event.request);
     })
   );
 });
-    
