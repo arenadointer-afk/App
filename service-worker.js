4765bf3e48409bb221e2125fb32a4ac9d0a2ca8c
arenadointer-afk <arenadointer@gmail.com>
2026-03-11 17:26:13 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "AtualizarApp_v3"; // MUDAR AQUI SEMPRE QUE ATUALIZAR O APP
+const CACHE_NAME = "AtualizarApp1"; // MUDAR AQUI SEMPRE QUE ATUALIZAR O APP
 
 const FILES_TO_CACHE = [
   "/App/",
